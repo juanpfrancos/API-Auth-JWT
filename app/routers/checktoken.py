be78e01e4d52183f67f06e3a721dcc049cbd264a
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2023-07-06 12:43:33 -0500

    Setup DockerFile and organize directory.

---
+++ app/routers/checktoken.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from models.user import User
+from schemas.user import user_schema
+from config import db_client, JWT_SECRET, JWT_ALGORITHM
+
+
+ACCESS_TOKEN_DURATION = 4
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+router = APIRouter(prefix="/check",
+                   tags=["Check"],
+                   responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
+
+
+
+def search_user(username: str):
+    user = User(**user_schema(db_client.users.find_one({"username": username})))
+    return user
+
+
+async def auth_user(token: str = Depends(oauth2_scheme)):
+    exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Credenciales de autenticación inválidas",
+        headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        username = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM]).get("sub")
+        if username is None:
+            raise exception
+    except JWTError:
+        raise exception
+    return search_user(username)
+
+
+async def current_user(user: User = Depends(auth_user)):
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
+    return user
+
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
+
+
---
+++ app/routers/signup.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException, status
+from models.user import User
+from schemas.user import user_schema
+from config import db_client, crypt_context
+
+
+router = APIRouter(prefix="/signup",
+   tags=["SignUp"],
+   responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
+
+
+@router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+async def user(user: User):
+    if type(search_user("email", user.email)) == User:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
+    try:
+        user_dict = dict(user)
+        user_dict.update({"password":crypt_context.encrypt(user.password)})
+        del user_dict["id"]
+        _id = db_client.users.insert_one(user_dict).inserted_id
+        new_user = user_schema(db_client.users.find_one({"_id": _id}))
+        return User(**new_user)
+    except Exception as e:
+        return {"error": f"User not created {str(e)}"}
+
+
+def search_user(field: str, key):
+    try:
+        user = db_client.users.find_one({field: key})
+        return User(**user_schema(user))
+    except Exception as e:
+        return {"error": f"User not found {str(e)}"}
